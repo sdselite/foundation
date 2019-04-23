00c42374d17425f5d79fcacf741d5868df8b65fe
Roy Lawson <relawson@sdselite.com>
2019-04-23 09:32:58 -0400

    Added password policy

---
+++ SDSFoundation.Model.Security.NetStandard/Policies/PasswordPolicy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SDSFoundation.Model.Security.Policies
+{
+    public class PasswordPolicy
+    {
+        private static int Minimum_Length = 7;
+        private static int Upper_Case_length = 1;
+        private static int Lower_Case_length = 1;
+        private static int NonAlpha_length = 1;
+
+        public static bool IsValid(string Password)
+        {
+            if (Password.Length < Minimum_Length)
+                return false;
+            if (UpperCaseCount(Password) < Upper_Case_length)
+                return false;
+            if (LowerCaseCount(Password) < Lower_Case_length)
+                return false;
+            if (NumericCount(Password) < 1)
+                return false;
+            if (NonAlphaCount(Password) < NonAlpha_length)
+                return false;
+            return true;
+        }
+
+        private static int UpperCaseCount(string Password)
+        {
+            return Regex.Matches(Password, "[A-Z]").Count;
+        }
+
+        private static int LowerCaseCount(string Password)
+        {
+            return Regex.Matches(Password, "[a-z]").Count;
+        }
+        private static int NumericCount(string Password)
+        {
+            return Regex.Matches(Password, "[0-9]").Count;
+        }
+        private static int NonAlphaCount(string Password)
+        {
+            return Regex.Matches(Password, @"[^0-9a-zA-Z\._]").Count;
+        }
+    }
+}
